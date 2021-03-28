aca965bde9697152db3da1ca6e1afff282698dc6
Simon Stolarczyk <simonstolarczyk@gmail.com>
2021-03-27 20:22:05 -0500

    Add user input.

--- wavemaker/src/main.rs
+++ wavemaker/src/main.rs
@@ -2,11 +2,36 @@ use std::f32::consts::PI;
 use std::fs::File;
 use std::path::Path;
 use itertools_num::linspace;
+use std::io;
+
+fn get_song_length() -> u32 {
+    let mut song_length = String::new();
+    println!("How long should the song be (in seconds)?");
+    io::stdin()
+        .read_line(&mut song_length)
+        .expect("Failed to get song length.");
+    let song_length: u32 = song_length.trim().parse().expect("Please type a number.");
+    song_length
+}
+
+fn get_song_frequencies() -> String {
+    let mut song_frequencies = String::new();
+    println!("Provide frequencies to play, separated by spaces.");
+    io::stdin()
+        .read_line(&mut song_frequencies)
+        .expect("Failed to get frequncies.");
+
+    let song_frequencies: String = song_frequencies.trim().expect("Please provide frequencies.");
+    song_frequencies
+}
 
 fn main() -> std::io::Result<()> {
+    let song_length: u32 = get_song_length();
+    println!("Song will be {} seconds long.", song_length);
+
+    // next we want the user to provide space-separated frequencies
     let freq: f32 = 400.0;
     let sample_rate: u32 = 44_100;
-    let song_length: u32 = 8;
 
     let one_sec: Vec<f32> = linspace::<f32>(0., 1., sample_rate as usize).collect();
     let mut x: Vec<f32> = linspace::<f32>(0., 1., sample_rate as usize).collect();
